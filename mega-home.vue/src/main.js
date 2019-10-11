58d1fe09654e36f117cb26cbd19fd455b0eb5489
Meidel Pérez Martin <msperez_profit@bankinter.com>
2019-10-12 00:42:54 +0200

    fix: vuetify dependencies

--- mega-home.vue/src/main.js
+++ mega-home.vue/src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import Vuelidate from "vuelidate";
+import Vuetify from "vuetify";
+import vuetify from "@/plugins/vuetify";
 
-Vue.config.productionTip = false
+import "material-design-icons/iconfont/material-icons.css";
+import "vuetify/dist/vuetify.css";
+
+Vue.use(Vuelidate);
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
+  vuetify,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
