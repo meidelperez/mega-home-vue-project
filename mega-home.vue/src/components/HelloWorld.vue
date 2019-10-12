e54abb960aad04f5f77e69cd999d83b84e703451
Meidel Pérez Martin <msperez_profit@bankinter.com>
2019-10-13 01:21:21 +0200

    feature:layout for the page

--- mega-home.vue/src/components/HelloWorld.vue
+++ mega-home.vue/src/components/HelloWorld.vue
@@ -1,12 +1,59 @@
 <template>
-  <v-content>
-    <v-btn>Hello!</v-btn>
-  </v-content>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item @click="onClick($event)">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="onClick($event)">
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center"></v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import { log } from "util";
 export default {
-  name: "HelloWorld"
+  name: "HelloWorld",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  }),
+  methods: {
+    onClick(event) {
+      event.stopPropagation();
+    }
+  }
 };
 </script>
 
